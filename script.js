6ef124ffd8d38ce1a0542b9f8dfea2e2ee6fba4c
Edluel <edluel_machado@hotmail.com>
2023-01-18 16:58:16 -0300

    mobile header and side menu implemented

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+//checing witch display should be used
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     var menu = document.getElementById("desktop");
-    menu.classList.add("hidden");  
+    menu.style.display = "none";
+    //define side menu margin-top
+    let header = document.getElementById("mobile");
+    let headerHeight = header.offsetHeight;
+    let sidemenu = document.getElementById("side-menu");
+    sidemenu.style.marginTop = headerHeight + "px";
+
   } else {
     var menu = document.getElementById("mobile");
-    menu.classList.add("hidden");
+    menu.style.display = "none";
+    var sm = document.getElementById("side-menu");
+    sm.style.display = "none";
 }
+
+//Enable open/close side menu
+const sideMenu = document.getElementById("hamburguer");
+const openSideMenu = document.getElementById("side-menu");
+
+sideMenu.addEventListener('click', () => {
+  if(openSideMenu.classList.contains("side-menu--open")){
+    openSideMenu.classList.remove("side-menu--open");
+  }
+  else {
+    openSideMenu.classList.add("side-menu--open");
+  }
+});
+
+
+
+
+
