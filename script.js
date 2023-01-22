daa737b7a9dbdec8869f2d65fe4bd3be071253ea
Edluel <edluel_machado@hotmail.com>
2023-01-22 19:50:27 -0300

    adding swiper function, navgation-menu dropdown and uploading screenshots

--- script.js
+++ script.js
@@ -7,13 +7,21 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     let headerHeight = header.offsetHeight;
     let sidemenu = document.getElementById("side-menu");
     sidemenu.style.marginTop = headerHeight + "px";
+    //hide slider arrowns
+    var sbl = document.getElementById("sbl");
+    var sbr = document.getElementById("sbr");
+    sbl.style.display = "none";
+    sbr.style.display = "none";
+
 
   } else {
     var menu = document.getElementById("mobile");
     menu.style.display = "none";
     var sm = document.getElementById("side-menu");
     sm.style.display = "none";
-}
+  }
+
+
 
 //Enable open/close side menu
 const sideMenu = document.getElementById("hamburguer");
@@ -29,6 +37,43 @@ sideMenu.addEventListener('click', () => {
 });
 
 
+
+//Enable dropdown menu
+const dropdown = document.querySelector('.navigation');
+
+dropdown.addEventListener('mouseenter', function() {
+  const dropdownContent = this.querySelector('.navigation-dropdown');
+  dropdownContent.style.display = 'block';
+});
+
+dropdown.addEventListener('mouseleave', function() {
+  const dropdownContent = this.querySelector('.navigation-dropdown');
+  dropdownContent.style.display = 'none';
+});
+
+
+//Enable Slider
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  spaceBetween: 5,
+  slidesPerView: 2,
+  loop: false,
+  freeMode: true,
+  speed: 500,
+  // Navigation arrows
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+      // when window width is >= 640px
+      640: {
+          slidesPerView: 5,
+          slidesPerGroup: 5,
+          freeMode: false
+      }
+  }
+})
 
 
 
